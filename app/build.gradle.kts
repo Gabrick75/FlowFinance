b055c330acb04030c34c9f377c75fa5a0dd9cd47
Gabrick75 <gabriel.dijair.souza@gmail.com>
2026-01-07 22:36:07 -0300

    Bump app version to 2.0.0 and remove unused imports
    - Update `versionName` to "2.0.0" and `versionCode` to 4 in `build.gradle.kts`.
    - Update `app_version_name` string resource in default, Spanish, and Portuguese localization files.
    - Removed unused things

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.flowfinance.app"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
